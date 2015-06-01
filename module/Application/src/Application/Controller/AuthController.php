a315d3516c64bb4851a59460f09ee65ff06876d5
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2015-05-31 23:28:50 -0500

    Initial code of JWT was added.

--- module/Application/src/Application/Controller/AuthController.php
+++ module/Application/src/Application/Controller/AuthController.php
@@ -3,6 +3,7 @@
 
 namespace Application\Controller;
 
+use JWT;
 use Zend\Mvc\Controller\AbstractActionController;
 
 class AuthController extends AbstractActionController
@@ -11,6 +12,16 @@ class AuthController extends AbstractActionController
     {
         if ($this->getRequest()->isPost())
         {
+            // $data = $this->getRequest()->getPost()->toArray();
+
+            $user = (object) array ('iat' => time(), 'exp' => time() + 300);
+            $jwt = JWT::encode($user, '');
+
+            $response = $this->getResponse();
+            $response->setContent(json_encode (array ('code' => 0,'response' => array ('token' => $jwt))));
+            $response->getHeaders()->clearHeaders()->addHeaderLine('Content-Type', 'application/json');
+
+            return $this->response;
         }
         else
             return array ('form' => new \Application\Form\Login());

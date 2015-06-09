a643228d4656d32903c73d86140447b85d8e5379
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2015-06-09 12:05:13 -0500

    Method to login was implemented.
    This method need to be tested.

--- module/Application/src/Application/Controller/AuthController.php
+++ module/Application/src/Application/Controller/AuthController.php
@@ -10,20 +10,31 @@ class AuthController extends AbstractActionController
 {
     public function loginAction()
     {
+        $response = $this->getResponse();
+        $response->getHeaders()->clearHeaders()->addHeaderLine('Content-Type', 'application/json');
+
         if ($this->getRequest()->isPost())
         {
-            // $data = $this->getRequest()->getPost()->toArray();
+            $data = $this->getRequest()->getPost()->toArray();
+            $dbAdapter = $this->getServiceLocator()->get('Zend\Db\Adapter');
 
-            $user = (object) array ('iat' => time(), 'exp' => time() + 300);
-            $jwt = JWT::encode($user, '');
+            $authAdapter = new AuthAdapter($dbAdapter, 'user', 'username', 'password');
+            $authAdapter->setIdentity($data['username'])->setCredential(sha1($data['password']));
+            $result = $auth->authenticate($authAdapter);
 
-            $response = $this->getResponse();
-            $response->setContent(json_encode (array ('code' => 0,'response' => array ('token' => $jwt))));
-            $response->getHeaders()->clearHeaders()->addHeaderLine('Content-Type', 'application/json');
+            if ($result->isValid())
+            {
+                $user = (object) array ('iat' => time(), 'exp' => time() + 300, 'user' => $authAdapter->getResultRowObject(array ('id', 'username')));
+                $jwt = JWT::encode($user, 'auth-example');
 
-            return $this->response;
+                $response->setContent(json_encode (array ('code' => 0, 'token' => $jwt)));
+            }
+            else
+                $response->setContent(json_encode (array ('code' => 1, 'token' => null)));
         }
         else
-            return array ('form' => new \Application\Form\Login());
+            $response->setContent(json_encode (array ('code' => 2, 'token' => null)));
+
+        return $this->response;
     }
 }
